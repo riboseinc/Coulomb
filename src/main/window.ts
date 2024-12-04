0dfb591c93fa01032f91914fe984255c50be357c
Wang <146128882+RED-ROSE515@users.noreply.github.com>
2024-12-05 01:09:02 +0200

    fix window

--- src/main/window.ts
+++ src/main/window.ts
@@ -1,18 +1,17 @@
-import * as path from 'path'
-import { format as formatUrl } from 'url';
+import * as path from "path";
+import { format as formatUrl } from "url";
 import {
   app,
   protocol,
   BrowserWindow,
   Menu,
   MenuItemConstructorOptions,
-} from 'electron';
+} from "electron";
 
-import { AppConfig } from '../config/app';
+import { AppConfig } from "../config/app";
 
-
-const isDevelopment = process.env.NODE_ENV !== 'production';
-const isMacOS = process.platform === 'darwin';
+const isDevelopment = process.env.NODE_ENV !== "production";
+const isMacOS = process.platform === "darwin";
 
 // Keeps track of windows and ensures (?) they do not get garbage collected
 export var windows: BrowserWindow[] = [];
@@ -20,50 +19,55 @@ export var windows: BrowserWindow[] = [];
 // Allows to locate window ID by label
 var windowsByTitle: { [title: string]: BrowserWindow } = {};
 
-
 app.whenReady().then(() => {
-  protocol.registerFileProtocol('file', (request, cb) => {
-    const components = request.url.replace('file:///', '').split('?', 2);
+  protocol.registerFileProtocol("file", (request, cb) => {
+    const components = request.url.replace("file:///", "").split("?", 2);
     if (isDevelopment) {
       cb(components.map(decodeURI)[0]);
     } else {
-      cb(components.map(decodeURI).join('?'));
+      cb(components.map(decodeURI).join("?"));
     }
   });
 });
 
-
 // Open new window, or focus if one with the same title already exists
 export interface WindowOpenerParams {
-  title: string
-  url?: string
-  component?: string
-  componentParams?: string
+  title: string;
+  url?: string;
+  component?: string;
+  componentParams?: string;
   dimensions?: {
-    minHeight?: number
-    minWidth?: number
-    height?: number
-    width?: number
-    maxHeight?: number
-    maxWidth?: number
-  }
-  frameless?: boolean
-  winParams?: any
-  menuTemplate?: MenuItemConstructorOptions[]
-  ignoreCache?: boolean
-  showWhileLoading?: boolean
-  config: AppConfig
+    minHeight?: number;
+    minWidth?: number;
+    height?: number;
+    width?: number;
+    maxHeight?: number;
+    maxWidth?: number;
+  };
+  frameless?: boolean;
+  winParams?: any;
+  menuTemplate?: MenuItemConstructorOptions[];
+  ignoreCache?: boolean;
+  showWhileLoading?: boolean;
+  config: AppConfig;
 }
-export type WindowOpener = (props: WindowOpenerParams) => Promise<BrowserWindow>;
+export type WindowOpener = (
+  props: WindowOpenerParams
+) => Promise<BrowserWindow>;
 export const openWindow: WindowOpener = async ({
-    title,
-    url, component, componentParams,
-    dimensions, frameless,
-    winParams, menuTemplate, ignoreCache,
-    showWhileLoading,
-    config }) => {
-
-  if ((component || '').trim() === '' && (url || '').trim() === '') {
+  title,
+  url,
+  component,
+  componentParams,
+  dimensions,
+  frameless,
+  winParams,
+  menuTemplate,
+  ignoreCache,
+  showWhileLoading,
+  config,
+}) => {
+  if ((component || "").trim() === "" && (url || "").trim() === "") {
     throw new Error("openWindow() requires either `component` or `url`");
   }
 
@@ -75,7 +79,7 @@ export const openWindow: WindowOpener = async ({
   }
 
   const _framelessOpts = {
-    titleBarStyle: isMacOS ? 'hiddenInset' : undefined,
+    titleBarStyle: isMacOS ? "hiddenInset" : undefined,
   };
 
   const _winParams = {
@@ -90,22 +94,22 @@ export const openWindow: WindowOpener = async ({
   let window: BrowserWindow;
 
   if (component) {
-    const params = `c=${component}&${componentParams ? componentParams : ''}`;
+    const params = `c=${component}&${componentParams ? componentParams : ""}`;
     window = await createWindowForLocalComponent(
       title,
       params,
       _winParams,
       showWhileLoading === true,
-      config.forceDevelopmentMode || false);
-
+      config.forceDevelopmentMode || false
+    );
   } else if (url) {
     window = await createWindow(
       title,
       url,
       _winParams,
       showWhileLoading === true,
-      ignoreCache);
-
+      ignoreCache
+    );
   } else {
     throw new Error("Either component or url must be given to openWindow()");
   }
@@ -120,16 +124,17 @@ export const openWindow: WindowOpener = async ({
 
   windows.push(window);
   windowsByTitle[title] = window;
-  window.on('closed', () => { delete windowsByTitle[title]; cleanUpWindows(); });
+  window.on("closed", () => {
+    delete windowsByTitle[title];
+    cleanUpWindows();
+  });
 
   return window;
-}
-
+};
 
 export function getWindowByTitle(title: string): BrowserWindow | undefined {
   return windowsByTitle[title];
 }
-
 
 export function closeWindow(title: string) {
   const win = getWindowByTitle(title);
@@ -138,12 +143,11 @@ export function closeWindow(title: string) {
   }
 }
 
-
-export function getWindow(func: (win: BrowserWindow) => boolean): BrowserWindow | undefined {
+export function getWindow(
+  func: (win: BrowserWindow) => boolean
+): BrowserWindow | undefined {
   return windows.find(func);
 }
-
-
 
 // Iterate over array of windows and try accessing window ID.
 // If it throws, window was closed and we remove it from the array.
@@ -164,34 +168,38 @@ function cleanUpWindows() {
   }
 }
 
-
 async function createWindowForLocalComponent(
-    title: string,
-    params: string,
-    winParams: any,
-    showWhileLoading: boolean,
-    forceDebug: boolean): Promise<BrowserWindow> {
-
+  title: string,
+  params: string,
+  winParams: any,
+  showWhileLoading: boolean,
+  forceDebug: boolean
+): Promise<BrowserWindow> {
   let url: string;
 
   if (isDevelopment) {
     url = `http://localhost:${process.env.ELECTRON_WEBPACK_WDS_PORT}?${params}`;
-  }
-  else {
+  } else {
     url = `${formatUrl({
-      pathname: path.join(__dirname, 'index.html'),
-      protocol: 'file',
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file",
       slashes: true,
     })}?${params}`;
   }
 
-  const window = await createWindow(title, url, winParams, showWhileLoading, forceDebug || isDevelopment);
+  const window = await createWindow(
+    title,
+    url,
+    winParams,
+    showWhileLoading,
+    forceDebug || isDevelopment
+  );
 
   if (forceDebug || isDevelopment) {
-    window.webContents.on('devtools-opened', () => {
+    window.webContents.on("devtools-opened", () => {
       window.focus();
       setImmediate(() => {
-        window.focus()
+        window.focus();
       });
     });
     window.webContents.openDevTools();
@@ -200,27 +208,27 @@ async function createWindowForLocalComponent(
   return window;
 }
 
-
 async function createWindow(
-    title: string,
-    url: string,
-    winParams: any,
-    showWhileLoading: boolean,
-    debug: boolean = false): Promise<BrowserWindow> {
-
+  title: string,
+  url: string,
+  winParams: any,
+  showWhileLoading: boolean,
+  debug: boolean = false
+): Promise<BrowserWindow> {
   const window = new BrowserWindow({
     webPreferences: {
       nodeIntegration: true,
       webSecurity: !debug,
       enableRemoteModule: true,
+      contextIsolation: false,
     },
     title: title,
     show: showWhileLoading === true,
-    ...winParams
+    ...winParams,
   });
 
   const promise = new Promise<BrowserWindow>((resolve, reject) => {
-    window.once('ready-to-show', () => {
+    window.once("ready-to-show", () => {
       if (showWhileLoading !== true) {
         window.show();
       }
@@ -229,7 +237,7 @@ async function createWindow(
   });
 
   if (debug) {
-    window.loadURL(url, {'extraHeaders': 'pragma: no-cache\n'});
+    window.loadURL(url, { extraHeaders: "pragma: no-cache\n" });
   } else {
     window.loadURL(url);
   }
@@ -237,18 +245,22 @@ async function createWindow(
   return promise;
 }
 
-
 export async function notifyAllWindows(eventName: string, payload?: any) {
-  await Promise.all(windows.map(async (window) => {
-    if (window) {
-      await window.webContents.send(eventName, payload);
-    }
-    return;
-  }));
+  await Promise.all(
+    windows.map(async (window) => {
+      if (window) {
+        await window.webContents.send(eventName, payload);
+      }
+      return;
+    })
+  );
 }
 
-
-export async function notifyWindow(windowTitle: string, eventName: string, payload?: any) {
+export async function notifyWindow(
+  windowTitle: string,
+  eventName: string,
+  payload?: any
+) {
   const window = getWindowByTitle(windowTitle);
   if (window) {
     await window.webContents.send(eventName, payload);
